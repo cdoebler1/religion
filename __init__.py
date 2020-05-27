d7c8af72f09ec6a760803f36ded4e41209f05eb3
cdoebler1 <cdoebler1@gmail.com>
2020-05-27 15:52:29 -0400

    Quick fix

--- __init__.py
+++ __init__.py
@@ -22,10 +22,9 @@ class Religion(MycroftSkill):
             self.speak("From the Dude de Ching, Verse " + str(a))
             self.speak(book)
 
-    @intent_handler(IntentBuilder('ReciteDDC').require('recite').
+    @intent_handler(IntentBuilder('ReciteIRRUD').require('recite').
                     require('irrud'))
     def handle_irrud(self, message):
-        self.speak_dialog('intro')
         a = random.randint(1, 8)
         with open('skills/religion.cdoebler1/pasta/irrud_' + str(a)
                   + '.txt') as file_object:
