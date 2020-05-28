57432150086da7af3f5511cafdb366384d9409c7
cdoebler1 <cdoebler1@gmail.com>
2020-05-28 15:25:47 -0400

    Enable reciting all verses

--- __init__.py
+++ __init__.py
@@ -27,19 +27,31 @@ class Religion(MycroftSkill):
                     require('irrud'))
     def handle_irrud(self, message):
         a = self.get_response('What verse would you like to hear?')
-        try:
-            a = w2n.word_to_num(a)
-        except Exception:
-            a = random.randint(1, 8)
-        if a < 1 or a > 8:
-            a = random.randint(1, 8)
-        with open('skills/religion.cdoebler1/pasta/irrud_' + str(a)
-                  + '.txt') as file_object:
-            book = file_object.read()
-            self.speak("""From the Gospel of the Flying Spaghetti Monster,
-            I'd really rather you didn't, number """
-                       + str(a))
-            self.speak(book)
+        if a == "all" or "everything":
+            i = 1
+            while i < 9:
+                with open('skills/religion.cdoebler1/pasta/irrud_' + str(a)
+                          + '.txt') as file_object:
+                    book = file_object.read()
+                    self.speak("""From the Gospel of the Flying Spaghetti Monster,
+                    I'd really rather you didn't, number """
+                               + str(a))
+                    self.speak(book)
+                i = i + 1
+        else:
+            try:
+                a = w2n.word_to_num(a)
+            except Exception:
+                a = random.randint(1, 8)
+            if a < 1 or a > 8:
+                a = random.randint(1, 8)
+            with open('skills/religion.cdoebler1/pasta/irrud_' + str(a)
+                      + '.txt') as file_object:
+                book = file_object.read()
+                self.speak("""From the Gospel of the Flying Spaghetti Monster,
+                I'd really rather you didn't, number """
+                           + str(a))
+                self.speak(book)
 
     def stop(self):
         pass
